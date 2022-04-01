02b45d4e08b8f27cc24d153546b9c6195760ae31
EmilCanAttack <90251019+EmilCanAttack@users.noreply.github.com>
2022-04-01 14:49:03 +0200

    Update port.js

--- port.js
+++ port.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = 2630;
 
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
+
+
+
 app.listen(port, () => {
     console.log(`Lytter på: http://localhost:${port}`)
 })
+
+
